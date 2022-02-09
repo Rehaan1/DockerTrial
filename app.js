3a6ad515392422b4ed38e1ca55c932819adc63c5
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2022-02-09 21:49:58 +0530

    feat: adding dev script

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get("/",(req,res) =>{
-    res.send("<h2>Hi There!! <h2>")
+    res.send("<h2>Hi There!!!!! <h2>")
 })
 
 app.listen(port, () => console.log(`listening on port ${port}`))

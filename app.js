54a3bf9a82724c00cbbfd5271ef3b27c73d0b16e
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2022-02-10 18:36:41 +0530

    feat: implement redis session

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config")
+const session = require("express-session")
+const redis = require("redis")
+let RedisStore = require("connect-redis")(session)
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, REDIS_URL, SESSION_SECRET, REDIS_PORT } = require("./config/config")
+
+let redisClient = redis.createClient({
+    host: REDIS_URL,
+    port: REDIS_PORT 
+})
 
 const postRouter = require("./routes/postRoutes")
+const userRouter = require("./routes/userRoutes")
 const app = express()
+
+app.use(session({
+    store: new RedisStore({
+        client: redisClient
+    }),
+    secret: SESSION_SECRET,
+    cookie:{
+        secure: false,
+        saveUninitialized: false,
+        resave: false,
+        httpOnly: true,
+        maxAge: 30000000
+    }
+}))
 
 const mongoURL=`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 
@@ -20,8 +43,12 @@ const connectWithRetry = () => {
 
 connectWithRetry()
 
+
+
+
 app.use(express.json())
 app.use("/posts",postRouter)
+app.use("/users",userRouter)
 
 const port = process.env.PORT || 3000
 
